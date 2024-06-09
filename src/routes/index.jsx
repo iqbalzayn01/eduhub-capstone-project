e58f3a325104b8046de341c91a055826ff9a7a15
fajrulfahmi <fajrulfahmi2002@gmail.com>
2024-06-09 11:37:58 +0700

    update:addEvent

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,6 +7,8 @@ import {
 import SignUp from '../pages/signup';
 import SignIn from '../pages/signin';
 import HomePage from '../pages/home';
+import AddEvent from '../pages/addEvent';
+import DetailEvent from '../pages/detailEvent';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -14,6 +16,8 @@ export const router = createBrowserRouter(
       <Route path="/" element={<HomePage />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
-    </>
-  )
+      <Route path="/addevent" element={<AddEvent />} />
+      <Route path="/detailevent" element={<DetailEvent />} />
+    </>,
+  ),
 );
