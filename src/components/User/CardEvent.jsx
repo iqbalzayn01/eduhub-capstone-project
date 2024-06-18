1449bf057d53c9e930b0be373847a4407f077a62
fajrulfahmi <fajrulfahmi2002@gmail.com>
2024-06-18 11:00:29 +0700

    update: styling

--- src/components/User/CardEvent.jsx
+++ src/components/User/CardEvent.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const CardEvent = ({ event }) => {
   return (
-    <div className="flex flex-col gap-5 lg:w-[35rem] h-full bg-slate-950 rounded-lg">
+    <div className="flex flex-col gap-5 lg:w-[35rem] min-h-120 min-w-96 bg-slate-950 rounded-lg ">
       <article
         className={`p-4 lg:p-10 flex-1 bg-[url(${event.bannerUrl})] bg-center bg-cover rounded-2xl overflow-hidden text-white`}
       >
